e919a54c8c42fe902499a0bd8d977110af97a5ae
k2matu <kstt90@gmail.com>
2024-01-09 12:06:22 +0200

    lis working and norm fixex

--- lis.c
+++ lis.c
@@ -6,35 +6,16 @@
 /*   By: kmatjuhi <kmatjuhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 12:48:12 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/01/09 10:11:06 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/01/09 12:05:48 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	*lis(int *arr, int len)
+static int	*create_lis(int *t, int *arr, int max, int i)
 {
-	int t[len];
-	int *lis;
-	int j = 0;
-	int max = 1;
-	int i = -1;
-	
-	while (i++ < len - 1)
-		t[i] = 1;
-	i = 0;
-	while (i++ < len - 1)
-	{
-		while (j < i)
-		{
-			if (arr[j] < arr[i] && t[i] < t[j] + 1)
-				t[i] = t[j] + 1;
-			if (t[i] > max)
-				max = t[i];
-			j++;
-		}
-		j = 0;
-	}
+	int	*lis;
+
 	lis = (int *)malloc(sizeof(int) * max);
 	while (max != 0)
 	{
@@ -45,5 +26,47 @@ int	*lis(int *arr, int len)
 		}
 		i--;
 	}
+	return (lis);
+}
+
+static int	*fill_t(int len)
+{
+	int	i;
+	int	*t;
+
+	i = -1;
+	t = (int *)malloc(sizeof(int) * len);
+	if (!t)
+		exit(-1);
+	while (i++ < len - 1)
+		t[i] = 1;
+	return (t);
+}
+
+int	*lis(int *arr, int len)
+{
+	int	*t;
+	int	i;
+	int	j;
+	int	max;
+	int	*lis;
+
+	max = 1;
+	i = 0;
+	t = fill_t(len);
+	while (i++ < len - 1)
+	{
+		j = 0;
+		while (j < i)
+		{
+			if (arr[j] < arr[i] && t[i] < t[j] + 1)
+				t[i] = t[j] + 1;
+			if (t[i] > max)
+				max = t[i];
+			j++;
+		}
+	}
+	lis = create_lis(t, arr, max, i);
+	free(t);
 	return (lis);
 }
--- sort_big_algo.c
+++ sort_big_algo.c
@@ -6,31 +6,45 @@
 /*   By: kmatjuhi <kmatjuhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 10:50:16 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/01/09 10:56:25 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/01/09 11:16:24 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_big(t_stack **a, int count)
+int	*lst_to_arr(t_stack **a, int count)
 {
 	int		*arr;
-	int		*l;
 	int		i;
-	int		j;
 	t_stack	*temp;
-	t_stack	*b;
 
-	b = NULL;
 	i = 0;
-	j = 0;
 	temp = *a;
 	arr = (int *)malloc(sizeof(int) * count);
+	if (!arr)
+	{
+		free_stack(a);
+		exit (-1);
+	}
 	while (i < count)
 	{
 		arr[i++] = temp->data;
 		temp = temp->next;
 	}
+	return (arr);
+}
+
+void	sort_big(t_stack **a, int count)
+{
+	int		*l;
+	int		*arr;
+	int		j;
+	int		i;
+	t_stack	*b;
+
+	b = NULL;
+	j = 0;
+	arr = lst_to_arr(a, count);
 	l = lis(arr, count);
 	i = 0;
 	while (i < count)
